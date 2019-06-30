348808c1094a364035c85f7ff7979080d02d76d2
ToncuElenaSonia <sonya_helena@yahoo.com>
2019-06-30 12:50:11 +0300

    rabbit homework with changes

---
+++ Rabbit/Rabbit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Rabbit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            RabbitClass myRabbit = new RabbitClass(EyesType.Red,FurType.White,GenderType.Female, new DateTime(1989,10,20));
+            Console.WriteLine("My rabbit looks like this:");
+            Console.WriteLine(myRabbit.AboutRabbit);
+            Console.WriteLine();
+            myRabbit.MoveRabbit();
+            Console.WriteLine();
+            myRabbit.SleepRabbit();
+            Console.WriteLine();
+            myRabbit.EatRabbit();
+        }
+    }
+}
---
+++ Rabbit/Rabbit/RabbitClass.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Rabbit
+{
+    public enum EyesType
+    {
+        Blue,
+        Red,
+        Black
+    }
+    public enum FurType
+    {
+        White,
+        Brown,
+        Black,
+        Grey
+    }
+    public enum GenderType
+    {
+        Male,
+        Female
+    }
+    class RabbitClass
+    {
+        
+        private int ageOfRabbit;
+        public EyesType EyesRabbit { get; set; }
+        public FurType FurOfRabbit { get; set; }
+        public GenderType Gender { get; set; }
+        public DateTime BirthdayRabbit { get; }
+        public int AgeOfRabbit
+        {
+            get
+            {
+                ageOfRabbit = DateTime.Now.Year - BirthdayRabbit.Year;
+                return ageOfRabbit;
+            }     
+        }
+        public RabbitClass(EyesType eyesRabbit, FurType furOfRabbit, GenderType gender, DateTime birthdayRabbit )
+        {
+            EyesRabbit = eyesRabbit;
+            FurOfRabbit = furOfRabbit;
+            Gender = gender;
+            BirthdayRabbit = birthdayRabbit;
+        }
+        //•	a rabbit can have Blue, Red or Black eyes
+        //•	a rabbit's fur can be white, brown, black or grey
+        //•	it has a gender
+        //•	has a birth date, and based on that you should be able to see how old the rabbit is
+        public string AboutRabbit
+        {
+            get
+            {
+                return $"My rabbit has {EyesRabbit} eyes, {FurOfRabbit} fur and {Gender} gender and it has {AgeOfRabbit} years";
+            }
+        }
+        
+        //•	we also know that a rabbit is a mammal that moves, sleeps and eats
+        public void MoveRabbit()
+        {
+            Console.WriteLine($"My rabbit can move.");
+        }
+        public void SleepRabbit()
+        {
+            Console.WriteLine($"My rabbit can sleep.");
+        }
+        public void EatRabbit()
+        {
+            Console.WriteLine($"My rabbit likes to eat alot.");
+        }
+    }
+}
+
